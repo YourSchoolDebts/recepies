58a40bc5a83d3e5d98df5bfe819e063726878f67
YourSchoolDebts <78000131+YourSchoolDebts@users.noreply.github.com>
2025-05-20 13:08:22 +0300

    Update nomatch.jsx

--- src/nomatch.jsx
+++ src/nomatch.jsx
@@ -1,1 +1,13 @@
 
+function NoMatch() {
+  let location = useLocation();
+
+  return (
+    <div>
+        <h1>Oops!</h1>
+        <h3>Sorry, an unexpected error has occurred.</h3>
+        <h5>Does not exist: {location.pathname}</h5>
+    </div>
+  );
+}
+
